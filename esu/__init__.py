fb53b1bd354a0201c43eed711a706254a53daeb6
Uatapatau <vovabandov@gmail.com>
2023-01-31 16:26:02 +0300

    fix: version 0.1.7

--- esu/__init__.py
+++ esu/__init__.py
@@ -1,7 +1,12 @@
 from .client import Client
 from .disk import Disk
+from .dns import Dns
+from .dns_record import DnsRecord
 from .firewall_template import FirewallTemplate
+from .firewall_template_rule import FirewallTemplateRule
 from .hypervisor import Hypervisor
+from .lbaas import Lbaas
+from .lbaas_pool import LbaasPool
 from .manager import Manager
 from .network import Network
 from .port import Port
@@ -15,4 +20,4 @@ from .vdc import Vdc
 from .vm import Vm
 from .vm_metadata import VmMetadata
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
