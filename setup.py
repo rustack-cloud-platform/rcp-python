a27c719d67e7b34262c8ecc050aedcaf36cc9eba
Andrey <akillon@mail.ru>
2024-02-22 01:42:24 +0300

    fix readthedocs

--- setup.py
+++ setup.py
@@ -13,18 +13,18 @@ with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='rustack-esu',
+    name='rcp-python',
     version=version,
-    description='RUSTACK-ESU Cloud API Wrapper',
+    description='Rustack Cloud Platform API Wrapper',
     long_description=long_description,
-    url='https://github.com/pilat/rustack-esu',
-    download_url='https://github.com/pilat/rustack-esu/tarball/{0}'\
+    url='https://github.com/rustack-cloud-platform/rcp-python',
+    download_url='https://github.com/rustack-cloud-platform/rcp-python/tarball/{0}'\
         .format(version),
     author='Vladimir K Urushev',
     author_email='urushev@yandex.ru',
     maintainer='Vladimir K Urushev',
     maintainer_email='urushev@yandex.ru',
-    keywords=['cloud', 'api'],
+    keywords=['cloud', 'api', 'python'],
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -49,6 +49,7 @@ setup(
         'dev': [
             "responses",
             "yapf==0.30.0",
+            "sphinx_rtd_theme==2.0.0",
             "pylint",
             "isort>=5.1.1 ; python_version>='3.6'",
             "tox>=3.9.0",
